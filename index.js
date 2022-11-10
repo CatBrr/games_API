7e9fb1ed92b5b0bff1aa4fe4b6438b76b6b33e45
Opilane TTHK <opilane@tthk.ee>
2022-11-10 13:15:36 +0200

    .

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const app = require('express')(); //impordib node_modules/ kaustas oleva express/ kausta, kus asub Express raamistik. Tühi sulupaar rea lõpus käivitab mooduli sees oleva koodi, mis tagastab objekti, mille salvestame app nimelisse muutujasse.
+const express = require('express')(); //impordib node_modules/ kaustas oleva express/ kausta, kus asub Express raamistik. Tühi sulupaar rea lõpus käivitab mooduli sees oleva koodi, mis tagastab objekti, mille salvestame app nimelisse muutujasse.
+const app = express();
 const port = 8080 //Muutuja port väärtuseks paneme selle pordi numbri, millel soovime rakendust kuulama panna. Kasutame pordi määramiseks muutujat, et vajadusel saaks porti muuta ainult ühe muutuja väärtuse muutmisega.
 const swaggerUi = require('swagger-ui-express'); //laadib sisse swagger-ui-expressI raamistiku
 //const swaggerDocument = require('./docs/swagger.json'); //defineerib swaggeri dokumentatsiooni asukoha ning laadib selle sisse
 const yamljs=require('yamljs');//laadib sisse yamljs raamistiku
 const swaggerDocument = yamljs.load('./docs/swagger.yml'); //laaditakse .yaml fail
+
+app.use(express.json());
+
 const games=[ //lisame püsiprogrammeeritud massiiviga 8 mängu. Siinkohal saavad need, kes soovivad päris andmebaasiga projekti teha, kirjutada koodi, mis võtab games muutujasse andmed andmebaasist.
     {id:1,name:"Witcher 3",price:29.99},
     {id:2,name:"Cyberpunk 2077",price:59.99},
@@ -25,9 +29,28 @@ app.get('/games/:id', (req,res)=>{ //lisame /games/:id lõpp-punkti. See :id on 
     }
     res.send(games[req.params.id-1]); //saadame päringu vastusena tagasi games massiivist selle liikme, mille indeks on :id väärtus.
 });
+
+app.post('/games',(req,res)=>{ //lisatakse mängude massiivi uus objekt
+    if(!req.body.name || !req.body.price){
+        return res.status(400).send({ error: 'One or all params are missing'});
+    }
+    let game ={
+        id:games.lenght +1, //id väärtuseks on massiivi liikmete hetkearv + 1, 
+        price: req.body.price,//üritati lugeda .price attribuuti selliselt objektilt, mis pole defineeritud. index.js real 35 on price: req.body.price. Sellest saame tuletada, et see objekt, mis pole defineeritud, on req.body. 
+        name: req.body.name
+    };
+    games.push(game);
+    res.status(201)
+        .location(`${getBaseUrl(req)}/games/${games.length}`)
+        .send(game);
+});
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); //seadistatakse üles lõpp-punkt /docs, kus on lisatud swaggerUI
 
 //kutsume app muutujast välja meetodi listen() ja anname selle meetodi esimeseks argumendiks port muutujas oleva numbri, pannes sellega rakenduse kuulama võrgus seda porti sissetulevate päringute osas. 
 app.listen(port,()=> {
     console.log(`API up at: http://localhost:${port}`); //Teine, valikuline argument listen meetodil määratleb funktsiooni, mis läheb siis käima, kui rakendus on hakanud võrgus porti kuulama. Selles funktsioonis prindime me konsoolile aadressi, millel rakendus kättesaadav on. Nii on mugav aadressi brauserisse kopeerida ja osades terminalides (nagu nt VS Code) on aadress ka klõpsatav.
 });
+function getBaseUrl(req){
+    return req.connection && req.connection.encryptesd
+        ? 'https' : 'http' + `://${req.headers.host}`;
+}
